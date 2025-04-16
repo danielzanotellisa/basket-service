f6f4b81dbf4eee2fca58ec237aeacc97232f554f
Daniel Zanotelli <danielzanotelli12@gmail.com>
2025-04-16 09:39:38 -0300

    feat: Cria o service para consulta de uma API externa

---
+++ src/main/java/dev/jav/ecommerce/basketservice/service/ProductService.java
@@ -0,0 +1,18 @@
+package dev.jav.ecommerce.basketservice.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+
+    public void findAll() {
+
+
+    }
+
+    public void findById(Long id){}
+
+}
