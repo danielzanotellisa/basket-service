6575cba9eca145e2f781027c918521d1a52f97a0
Daniel Zanotelli <danielzanotelli12@gmail.com>
2025-04-16 10:31:06 -0300

    fix: Refatora os métodos de busca na API para que eles possam retornar o DTO de response e adiciona a anotação @PathVariable na busca por ID

--- src/main/java/dev/jav/ecommerce/basketservice/client/PlatziStoreClient.java
+++ src/main/java/dev/jav/ecommerce/basketservice/client/PlatziStoreClient.java
@@ -2,13 +2,16 @@ package dev.jav.ecommerce.basketservice.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @FeignClient(name = "PlatziStoreClient", url = "${basket.client.platzi}")
 public interface PlatziStoreClient {
 
     @GetMapping("/products")
-    public void getAllProducts();
+    public List<PlatziProductResponse> getAllProducts();
 
     @GetMapping("/products/{id}")
-    public void getProductById(Long id);
+    public PlatziProductResponse getProductById(@PathVariable Long id);
 }
