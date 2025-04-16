e4a2af452f18aeca00a8ba55fc7bf9052bb1416e
Daniel Zanotelli <danielzanotelli12@gmail.com>
2025-04-16 10:32:45 -0300

    fix: Refatora os métodos para que eles possam retornar o DTO de forma correta e faz injeção de dependência do FeignClient

--- src/main/java/dev/jav/ecommerce/basketservice/service/ProductService.java
+++ src/main/java/dev/jav/ecommerce/basketservice/service/ProductService.java
@@ -1,18 +1,26 @@
 package dev.jav.ecommerce.basketservice.service;
 
+import dev.jav.ecommerce.basketservice.client.PlatziProductResponse;
+import dev.jav.ecommerce.basketservice.client.PlatziStoreClient;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ProductService {
 
+    private final PlatziStoreClient platziStoreClient;
 
-    public void findAll() {
+    public List<PlatziProductResponse> findAll() {
 
-
+        return platziStoreClient.getAllProducts();
     }
 
-    public void findById(Long id){}
+    public PlatziProductResponse findById(Long id){
+        return platziStoreClient.getProductById(id);
+    }
+
 
 }
