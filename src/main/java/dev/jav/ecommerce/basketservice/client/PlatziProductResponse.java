3c629e0d3a69c9207340ce2b1a8765863813d56e
Daniel Zanotelli <danielzanotelli12@gmail.com>
2025-04-16 10:24:01 -0300

    fix: Arruma erro de digitação

--- src/main/java/dev/jav/ecommerce/basketservice/client/PlatziProductResponse.java
+++ src/main/java/dev/jav/ecommerce/basketservice/client/PlatziProductResponse.java
@@ -2,5 +2,5 @@ package dev.jav.ecommerce.basketservice.client;
 
 import java.math.BigDecimal;
 
-public record PlatziProdcutResponse(Long id, String title, BigDecimal price) {
+public record PlatziProductResponse(Long id, String title, BigDecimal price) {
 }
