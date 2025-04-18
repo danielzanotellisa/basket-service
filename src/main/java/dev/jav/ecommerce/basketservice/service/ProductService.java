65f49a543d3884717809f65cb53b2017ede080fe
Daniel Zanotelli <danielzanotelli12@gmail.com>
2025-04-18 11:23:12 -0300

    chore: Adiciona anotações no ProductService para criação de logs no console

--- src/main/java/dev/jav/ecommerce/basketservice/service/ProductService.java
+++ src/main/java/dev/jav/ecommerce/basketservice/service/ProductService.java
@@ -3,6 +3,7 @@ package dev.jav.ecommerce.basketservice.service;
 import dev.jav.ecommerce.basketservice.client.PlatziProductResponse;
 import dev.jav.ecommerce.basketservice.client.PlatziStoreClient;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -10,18 +11,20 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProductService {
 
     private final PlatziStoreClient platziStoreClient;
 
     @Cacheable(value = "products")
     public List<PlatziProductResponse> findAll() {
-
+        log.info("Getting all products");
         return platziStoreClient.getAllProducts();
     }
 
     @Cacheable(value = "product", key="#productId")
     public PlatziProductResponse findById(Long productId){
+        log.info("Getting product with ID: {}", productId);
         return platziStoreClient.getProductById(productId);
     }
 
