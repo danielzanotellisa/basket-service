1d283d1b6f1e9c701a9eaf94b684d3fbd81ead78
Daniel Zanotelli <danielzanotelli12@gmail.com>
2025-04-23 12:42:14 -0300

    feat: Cria a basket controller com o mapeamento de ending points

---
+++ src/main/java/dev/jav/ecommerce/basketservice/controller/BasketController.java
@@ -0,0 +1,38 @@
+package dev.jav.ecommerce.basketservice.controller;
+
+import dev.jav.ecommerce.basketservice.controller.request.BasketRequest;
+import dev.jav.ecommerce.basketservice.model.Basket;
+import dev.jav.ecommerce.basketservice.service.BasketService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/basket")
+@RequiredArgsConstructor
+public class BasketController {
+
+    private final BasketService basketService;
+
+    @PostMapping()
+    public ResponseEntity<Basket> createBasket(@RequestBody BasketRequest basket) {
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(basketService.createBasket(basket));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Basket>> findBasket(@PathVariable String id) {
+
+        Optional<Basket> optionalBasket = basketService.findBasket(id);
+        if(optionalBasket.isPresent()) {
+            return ResponseEntity.ok(optionalBasket);
+        }
+
+        else {
+            throw new IllegalArgumentException("O carrinho não foi encontrado");
+        }
+    }
+}
