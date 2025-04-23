f1cb3b8021f5c211e0e5c053e4c113b0877bc5ba
Daniel Zanotelli <danielzanotelli12@gmail.com>
2025-04-23 12:43:49 -0300

    feat: Cria DTOs para o product e a basket

---
+++ src/main/java/dev/jav/ecommerce/basketservice/controller/request/BasketRequest.java
@@ -0,0 +1,6 @@
+package dev.jav.ecommerce.basketservice.controller.request;
+
+import java.util.List;
+
+public record BasketRequest(Long clientId, List<ProductRequest> products) {
+}
---
+++ src/main/java/dev/jav/ecommerce/basketservice/controller/request/ProductRequest.java
@@ -0,0 +1,6 @@
+package dev.jav.ecommerce.basketservice.controller.request;
+
+import java.math.BigDecimal;
+
+public record ProductRequest(Long id, Integer quantity) {
+}
