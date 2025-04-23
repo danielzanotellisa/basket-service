8fe2797608e01fcbcb62d1f584d656488b515d89
Daniel Zanotelli <danielzanotelli12@gmail.com>
2025-04-23 12:41:30 -0300

    feat: Adiciona uma query customizada para busca por ID e STATUS no banco de dados

--- src/main/java/dev/jav/ecommerce/basketservice/repository/BasketRepository.java
+++ src/main/java/dev/jav/ecommerce/basketservice/repository/BasketRepository.java
@@ -1,9 +1,14 @@
 package dev.jav.ecommerce.basketservice.repository;
 
 import dev.jav.ecommerce.basketservice.model.Basket;
+import dev.jav.ecommerce.basketservice.model.Status;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BasketRepository extends MongoRepository<Basket, String> {
+
+  Optional<Basket> findByClientAndStatus(Long client, Status status);
 }
